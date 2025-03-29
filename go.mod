5b85bf59f6875812042fd3d9c9cf91bb6650a911
asherkhb <asherkhb@gmail.com>
2025-03-29 15:42:39 -0400

    Add config struct

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module asherkhb.com/ease
 
 go 1.24.0
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/caarlos0/env/v11 v11.3.1
+	github.com/gorilla/mux v1.8.1
+)
--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/gorilla/mux"
 )
+
+type Config struct {
+	Port int `env:"EASE_PORT" envDefault:"8080"`
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the home page!\n")
@@ -16,16 +22,20 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port := 8080
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("%+v", err)
+	}
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", home).Methods("GET")
 	r.HandleFunc("/login", login).Methods("GET")
 
-	fmt.Println("Listening on port:", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+	fmt.Println("Listening on port:", cfg.Port)
+	log.
+		err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r)
 	if err != nil {
-		fmt.Println("Error starting server: ", err)
+		log.Fatalf("Error starting server: %v", err)
 	}
 }
